355ff2a0573148a81d91c6942ca9874f9b445a83
Juan Pablo Giraldo Collazos <cpujuanpis@gmail.com>
2024-10-23 11:19:59 -0500

    ADD code for test

--- src/main/java/com/siasa/auth/Main.java
+++ src/main/java/com/siasa/auth/Main.java
@@ -1,7 +1,13 @@
 package com.siasa.auth;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello world!");
+        System.out.print("Ingrese su nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.println(String.format("Su nombre es %s", nombre));
     }
 }
